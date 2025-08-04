a191b2f1336364f5ebbe855b65d4dca1ff1f91eb
Drasenix <adowns@hotmail.fr>
2025-08-04 20:38:19 +0200

    more logs

--- enhancedQuestLog/scripts/enhancedQuestLog/player.lua
+++ enhancedQuestLog/scripts/enhancedQuestLog/player.lua
@@ -7,7 +7,10 @@ local currentQuests = {}
 function printQuests(quests)
   for id,quest in pairs(quests) do
       dialogueRecord = core.dialogue.journal.records[quest.id]
-      print("found member " .. id .. " -- " .. tostring(quest) .. " stage = " .. quest.stage .. " finished = " .. tostring(quest.finished) .. " started = " .. tostring(quest.started) .. " quest name = " .. tostring(dialogueRecord.questName));
+      print(id .. " -- " .. tostring(quest) .. " stage = " .. quest.stage .. " finished = " .. tostring(quest.finished) .. " started = " .. tostring(quest.started) .. " quest name = " .. tostring(dialogueRecord.questName));
+      for key,value in pairs(core.dialogue.journal.records[quest.id].infos) do
+        print(tostring(value.id) .. "  " .. tostring(value.questStage) .. "  " .. tostring(value.isQuestName) .. "   " .. value.text)
+      end
   end
 end
 
