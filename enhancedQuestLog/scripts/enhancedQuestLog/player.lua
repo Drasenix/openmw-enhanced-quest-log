117c9ced9b9ef11eb85e3a46e46f4efbe06108c5
Drasenix <adowns@hotmail.fr>
2025-08-04 19:57:40 +0200

    get started quests and unfinished and toggle them in log

--- enhancedQuestLog/scripts/enhancedQuestLog/player.lua
+++ enhancedQuestLog/scripts/enhancedQuestLog/player.lua
@@ -1,3 +1,23 @@
+local self = require("openmw.self")
+local types = require("openmw.types")
+
+local currentQuests = {}
+
+function printQuests(quests)
+  for id,quest in pairs(quests) do
+      print("found member " .. id .. " -- " .. tostring(quest) .. " stage = " .. quest.stage .. " finished = " .. tostring(quest.finished) .. " started = " .. tostring(quest.started));
+  end
+end
 
 
 
+local function getCurrentQuests()
+  for id,quest in pairs(types.Player.quests(self)) do
+    if quest.started and not quest.finished then
+      table.insert(currentQuests, quest)
+    end
+  end
+  printQuests(currentQuests)
+end
+
+getCurrentQuests()
