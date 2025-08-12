731e62a1842c2f46bc13871d4b50f0dfb4f2282c
Drasenix <adowns@hotmail.fr>
2025-08-12 21:10:25 +0200

    almost ok

--- takeAllEnhanced/scripts/takeAllEnhanced/player.lua
+++ takeAllEnhanced/scripts/takeAllEnhanced/player.lua
@@ -1,7 +1,61 @@
-function printContainerInventory()
+local I = require('openmw.interfaces')
+local types = require('openmw.types')
+local UI = require('openmw.interfaces').UI
+local configPlayer = require('scripts.takeAllEnhanced.config.player')
+local self = require('openmw.self')
+
+local currentContainer = nil
+local goldContainer = nil
+function uiModeChanged(e)
+	if e.newMode == I.UI.MODE.Container then
+		currentContainer = e.arg
+    getAllContainerGold()
+	else
+    currentContainer = nil
+		goldContainer = nil
+	end
+end
+
+function getAllContainerGold()
+  containerInventory = types.Container.inventory(currentContainer)
+  goldContainer = containerInventory:findAll('gold_001')
   return
 end
 
 function moveGoldFromContainerInventoryToPlayerInventory()
+  for _, item in ipairs(goldContainer) do
+    print(item.id)
+    item:moveInto(types.Actor.inventory(self.object))
+  end
   return
 end
+
+local function onKeyRelease(key)
+  local keyForTakeAllGold = configPlayer.options.s_Key_Gold
+  if UI.getMode() ~= "Container" then
+    return
+  end
+  if key.code == keyForTakeAllGold then
+    moveGoldFromContainerInventoryToPlayerInventory()
+  end
+end
+
+local function onMouseButtonPress(button)
+  local clickForTakeAllGold = configPlayer.options.s_Key_Gold
+  if UI.getMode() ~= "Container" then
+    return
+  end
+  if button == clickForTakeAllGold then
+    moveGoldFromContainerInventoryToPlayerInventory()
+  end
+end
+
+return {
+	eventHandlers = {
+		UiModeChanged = uiModeChanged
+	},
+  engineHandlers = {
+      onKeyRelease = onKeyRelease,
+      onMouseButtonPress = onMouseButtonPress,
+  }
+}
---
+++ takeAllEnhanced/scripts/takeAllEnhanced/settings/player.lua
@@ -0,0 +1,163 @@
+local core = require('openmw.core')
+local I = require('openmw.interfaces')
+local input = require('openmw.input')
+local ui = require('openmw.ui')
+local util = require('openmw.util')
+local async = require('openmw.async')
+local l10n = core.l10n('TakeAllEnhanced')
+local versionString = "1.0.0"
+
+-- inputSelection inspired by Pharis
+I.Settings.registerRenderer(
+    "inputSelection",
+	function(value, set)
+        local interval = {
+            type = ui.TYPE.Widget,
+            template = I.MWUI.templates.interval
+        }
+		local name = "No Key Set"
+		if value then
+            if value == 1 then
+                name = "Left Click"
+            elseif value == 2 then
+                name = "Middle Click"
+            elseif value == 3 then
+                name = "Right Click"
+            else
+                name = input.getKeyName(value)
+            end
+		end
+		return {
+            type = ui.TYPE.Flex,
+            props = {
+                horizontal = true
+            },
+			content = ui.content {
+                {
+                    type = ui.TYPE.Container,
+                    template = I.MWUI.templates.box,
+                    content = ui.content {
+                        {
+                            template = I.MWUI.templates.textEditLine,
+                            props = {
+                                text = name,
+                            },
+                            events = {
+                                keyPress = async:callback(function(e)
+                                    if e.code == input.KEY.Escape then return end
+                                    set(e.code)
+                                end),
+                            },
+                        },                                                                        
+                    }
+                },
+                interval,
+                interval,
+                interval,
+                {
+                    type = ui.TYPE.Container,
+                    template = I.MWUI.templates.box,
+                    content = ui.content {
+                        {
+                            template = I.MWUI.templates.textNormal,
+                            props = {
+                                text = " Tab ",
+                            },
+                            events = {
+                                mousePress = async:callback(function(e)
+                                    set(input.KEY.Tab)
+                                end),
+                            },
+                        }				
+                    }
+                },
+                interval,
+                interval,
+                interval,
+                {
+                    type = ui.TYPE.Container,
+                    template = I.MWUI.templates.box,
+                    content = ui.content {
+                        {
+                            template = I.MWUI.templates.textNormal,
+                            props = {
+                                text = " Left Click ",
+                            },
+                            events = {
+                                mousePress = async:callback(function(e)
+                                    set(1)
+                                end),
+                            },
+                        }				
+                    }
+                },
+                interval,
+                interval,
+                interval,
+                {
+                    type = ui.TYPE.Container,
+                    template = I.MWUI.templates.box,
+                    content = ui.content {
+                        {
+                            template = I.MWUI.templates.textNormal,
+                            props = {
+                                text = " Middle Click ",
+                            },
+                            events = {
+                                mousePress = async:callback(function(e)
+                                    set(2)
+                                end),
+                            },
+                        }				
+                    }
+                },
+                interval,
+                interval,
+                interval,              
+                {
+                    type = ui.TYPE.Container,
+                    template = I.MWUI.templates.box,
+                    content = ui.content {
+                        {
+                            template = I.MWUI.templates.textNormal,
+                            props = {
+                                text = " Right Click ",
+                            },
+                            events = {
+                                mousePress = async:callback(function(e)
+                                    set(3)
+                                end),
+                            },
+                        }				
+                    }
+                },
+			},            
+		}
+	end
+)
+
+-- Settings page
+I.Settings.registerPage {
+    key = 'TakeAllEnhanced',
+    l10n = 'TakeAllEnhanced',
+    name = 'ConfigTitle',
+    description = l10n('ConfigSummary'):gsub('%%{version}', versionString),
+}
+
+I.Settings.registerGroup {
+    key = 'Settings/TakeAllEnhanced/KeyBindings/Config',
+    page = 'TakeAllEnhanced',
+    l10n = 'TakeAllEnhanced',
+    name = 'ConfigKeybindings',
+    description = "ConfigKeybindingsDesc",
+    permanentStorage = true,
+    settings = {        
+        {
+            key = 's_Key_Gold',
+            renderer = 'inputSelection',            
+            name = 'Key_Gold',
+            description = 'Gold',
+            default = 0,
+        },
+    },
+}
