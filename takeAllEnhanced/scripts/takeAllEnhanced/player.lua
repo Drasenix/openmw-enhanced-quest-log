92ddc24a0e833891e56bedf534fc6bacc17dc7c0
Drasenix <adowns@hotmail.fr>
2025-08-19 00:27:20 +0200

    More types handling

--- takeAllEnhanced/scripts/takeAllEnhanced/player.lua
+++ takeAllEnhanced/scripts/takeAllEnhanced/player.lua
@@ -9,6 +9,18 @@ local currentContainer = nil
 local goldContainer = nil
 local itemsContainer = nil
 local ingredientsContainer = nil
+local booksContainer = nil
+local scrollsContainer = nil
+local weaponsContainer = nil
+local miscellaneousContainer = nil
+local lockpickContainer = nil
+local probeContainer = nil
+local potionContainer = nil
+local repairContainer = nil
+local clothingContainer = nil
+local armorContainer = nil
+local apparatusContainer = nil
+local lightContainer = nil
 
 function uiModeChanged(e)
 	if e.newMode == I.UI.MODE.Container then
@@ -19,6 +31,18 @@ function uiModeChanged(e)
 		goldContainer = nil
     itemsContainer = nil
     ingredientsContainer = nil
+    booksContainer = nil
+    scrollsContainer = nil
+    weaponsContainer = nil
+    miscellaneousContainer = nil
+    lockpickContainer = nil
+    probeContainer = nil
+    potionContainer = nil
+    repairContainer = nil
+    clothingContainer = nil
+    armorContainer = nil
+    apparatusContainer = nil
+    lightContainer = nil
 	end
 end
 
@@ -34,6 +58,55 @@ function getAllContainerIngredients()
   ingredientsContainer = containerInventory:getAll(types.Ingredient)
 end
 
+function getAllContainerBooks()
+  booksContainer = containerInventory:getAll(types.Books)
+end
+
+function getAllContainerScrolls()
+  scrollsContainer = containerInventory:getAll(types.Scrolls)
+end
+
+function getAllContainerWeapons()
+  weaponsContainer = containerInventory:getAll(types.Weapons)
+end
+
+function getAllContainerMiscellaneous()
+  miscellaneousContainer = containerInventory:getAll(types.Miscellaneous)
+end
+
+function getAllContainerLockpick()
+  lockpickContainer = containerInventory:getAll(types.Lockpick)
+end
+
+function getAllContainerProbe()
+  probeContainer = containerInventory:getAll(types.Probe)
+end
+
+function getAllContainerPotion()
+  potionContainer = containerInventory:getAll(types.Potion)
+end
+
+function getAllContainerRepair()
+  repairContainer = containerInventory:getAll(types.Repair)
+end
+
+function getAllContainerClothing()
+  clothingContainer = containerInventory:getAll(types.Clothing)
+end
+
+function getAllContainerArmor()
+  armorContainer = containerInventory:getAll(types.Armor)
+end
+
+function getAllContainerApparatus()
+  apparatusContainer = containerInventory:getAll(types.Apparatus)
+end
+
+function getAllContainerLight()
+  lightContainer = containerInventory:getAll(types.Light)
+end
+
+
 function takeAllItemFromContainerThenCloseIt(container)
   if container ~= nil then
     for _, item in ipairs(container) do
@@ -47,6 +120,18 @@ local function getContainersItem()
   getAllContainerGold()
   getAllContainerItems()
   getAllContainerIngredients()
+  getAllContainerBooks()
+  getAllContainerScrolls()
+  getAllContainerWeapons()
+  getAllContainerMiscellaneous()
+  getAllContainerLockpick()
+  getAllContainerProbe()
+  getAllContainerPotion()
+  getAllContainerRepair()
+  getAllContainerClothing()
+  getAllContainerArmor()
+  getAllContainerApparatus()
+  getAllContainerLight()
 end
 
 
@@ -57,7 +142,20 @@ local function checkEntryAndHandleTakeAll(code)
   getContainersItem()
   local keyForTakeAllItems = configPlayer.options.s_Key_All
   local keyForTakeAllGold = configPlayer.options.s_Key_Gold
-  local keyForTakeAllIngredients = configPlayer.options.s_Key_Ingredients  
+  local keyForTakeAllIngredients = configPlayer.options.s_Key_Ingredients
+  local keyForTakeAllBooks = configPlayer.options.s_Key_Books
+  local keyForTakeAllScrolls = configPlayer.options.s_Key_Scrolls
+  local keyForTakeAllWeapons = configPlayer.options.s_Key_Weapons
+  local keyForTakeAllMiscellaneous = configPlayer.options.s_Key_Miscellaneous
+  local keyForTakeAllLockpick = configPlayer.options.s_Key_Lockpick
+  local keyForTakeAllProbe = configPlayer.options.s_Key_Probe
+  local keyForTakeAllPotion = configPlayer.options.s_Key_Potion
+  local keyForTakeAllRepair = configPlayer.options.s_Key_Repair
+  local keyForTakeAllClothing = configPlayer.options.s_Key_Clothing
+  local keyForTakeAllArmor = configPlayer.options.s_Key_Armor
+  local keyForTakeAllApparatus = configPlayer.options.s_Key_Apparatus
+  local keyForTakeAllLight = configPlayer.options.s_Key_Light
+  
   if code == keyForTakeAllGold then
     takeAllItemFromContainerThenCloseIt(goldContainer)
   end
@@ -68,6 +166,54 @@ local function checkEntryAndHandleTakeAll(code)
 
   if code == keyForTakeAllIngredients then
     takeAllItemFromContainerThenCloseIt(ingredientsContainer)
+  end
+
+  if code == keyForTakeAllBooks then
+    takeAllItemFromContainerThenCloseIt(booksContainer)
+  end
+
+  if code == keyForTakeAllScrolls then
+    takeAllItemFromContainerThenCloseIt(scrollsContainer)
+  end
+
+  if code == keyForTakeAllWeapons then
+    takeAllItemFromContainerThenCloseIt(weaponsContainer)
+  end
+
+  if code == keyForTakeAllMiscellaneous then
+    takeAllItemFromContainerThenCloseIt(miscellaneousContainer)
+  end
+
+  if code == keyForTakeAllLockpick then
+    takeAllItemFromContainerThenCloseIt(lockpickContainer)
+  end
+
+  if code == keyForTakeAllProbe then
+    takeAllItemFromContainerThenCloseIt(probeContainer)
+  end
+
+  if code == keyForTakeAllPotion then
+    takeAllItemFromContainerThenCloseIt(potionContainer)
+  end
+
+  if code == keyForTakeAllRepair then
+    takeAllItemFromContainerThenCloseIt(repairContainer)
+  end
+
+  if code == keyForTakeAllClothing then
+    takeAllItemFromContainerThenCloseIt(clothingContainer)
+  end
+
+  if code == keyForTakeAllArmor then
+    takeAllItemFromContainerThenCloseIt(armorContainer)
+  end
+
+  if code == keyForTakeAllApparatus then
+    takeAllItemFromContainerThenCloseIt(apparatusContainer)
+  end
+
+  if code == keyForTakeAllLight then
+    takeAllItemFromContainerThenCloseIt(lightContainer)
   end
 
 end
